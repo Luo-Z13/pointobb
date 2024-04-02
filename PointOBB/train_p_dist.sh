9247742f8fcf8d6ab7d1860c518a6dbb762576ff
Luo-Z13 <luojunwei@whu.edu.cn>
2024-04-02 15:58:53 +0800

    refresh script

--- PointOBB/train_p_dist.sh
+++ PointOBB/train_p_dist.sh
@@ -6,8 +6,8 @@ export CUDA_VISIBLE_DEVICES=0,1
 
 tools/dist_train.sh --config configs2/pointobb/pointobb_r50_fpn_2x_dior.py 2 \
                     --work-dir xxx/work_dir/pointobb_r50_fpn_2x_dior\
-                    --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dota10_dist/pseudo_obb_result.json'
+                    --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dior/pseudo_obb_result.json'
 
 # tools/dist_train.sh configs2/pointobb/pointobb_r50_fpn_2x_dota10.py 2 \
 #                     --work-dir xxx/work_dir/pointobb_r50_fpn_2x_dota10\
-#                     --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dota10_dist/pseudo_obb_result.json'
+#                     --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dota10/pseudo_obb_result.json'
