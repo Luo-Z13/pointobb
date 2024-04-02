2cd03094cb7837d954e380215f6612c8d5d06ebc
Luo-Z13 <luojunwei@whu.edu.cn>
2024-04-02 16:04:43 +0800

    refresh script

--- PointOBB/train_p_dist.sh
+++ PointOBB/train_p_dist.sh
@@ -4,10 +4,12 @@ conda activate openmmlab
 
 export CUDA_VISIBLE_DEVICES=0,1
 
+WORK_DIR="xxx/work_dir/pointobb_r50_fpn_2x_dior"
 tools/dist_train.sh --config configs2/pointobb/pointobb_r50_fpn_2x_dior.py 2 \
-                    --work-dir xxx/work_dir/pointobb_r50_fpn_2x_dior\
-                    --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dior/pseudo_obb_result.json'
+                    --work-dir $WORK_DIR\
+                    --cfg-options evaluation.save_result_file='$WORK_DIR/pseudo_obb_result.json'
 
+# WORK_DIR="xxx/work_dir/pointobb_r50_fpn_2x_dior"
 # tools/dist_train.sh configs2/pointobb/pointobb_r50_fpn_2x_dota10.py 2 \
-#                     --work-dir xxx/work_dir/pointobb_r50_fpn_2x_dota10\
-#                     --cfg-options evaluation.save_result_file='xxx/work_dir/pointobb_r50_fpn_2x_dota10/pseudo_obb_result.json'
+#                     --work-dir $WORK_DIR\
+#                     --cfg-options evaluation.save_result_file='$WORK_DIR/pseudo_obb_result.json'
